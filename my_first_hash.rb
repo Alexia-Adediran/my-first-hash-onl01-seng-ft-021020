5da1f410f887c1c7377e0d20687978b4a94d91b6
Alexia Adediran <alexiaadediran30@gmail.com>
2020-02-14 16:43:53 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,7 +14,7 @@ shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil pai
 end
 
 def retrieval
-  shipping_manifest = {
+  shipping_manifest = 3 {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
